e30cc4e0c525290237f3020b8553ae236e1231fa
Soof Golan <soofgolan@gmail.com>
2023-02-11 16:35:41 +0200

    fix is_set c-api compatibility for python<=3.10

--- src/serialize/error.rs
+++ src/serialize/error.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 use crate::error::INVALID_STR;
-use pyo3_ffi::{PyErr_SetNone, PyErr_SetString, PyExc_RecursionError};
+use pyo3_ffi::{PyErr_SetNone, PyExc_RecursionError};
 use std::ffi::CStr;
 use std::ptr::NonNull;
 
